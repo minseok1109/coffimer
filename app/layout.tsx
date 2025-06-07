1794eb28110cbe252db35792129cabe0d25b673d
ms-bang <ms.bang@teamsparta.co>
2025-06-07 23:08:34 +0900

    feat: calrity 설치

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,6 +44,20 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
     return (
         <html lang="ko">
+            <head>
+                <script
+                    type="text/javascript"
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                    (function(c,l,a,r,i,t,y){
+                        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+                    })(window, document, "clarity", "script", "rvsqy2z045");
+                    `,
+                    }}
+                />
+            </head>
             <body className="">{children}</body>
         </html>
     );
