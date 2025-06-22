2e9c6dc2c8bb63cbe2d017fd7e1a604be736ac1d
ms-bang <ms.bang@teamsparta.co>
2025-06-22 14:13:56 +0900

    feat: add channelTalk

--- app/layout.tsx
+++ app/layout.tsx
@@ -57,6 +57,16 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
                     `,
                     }}
                 />
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `
+            (function(){var w=window;if(w.ChannelIO){return w.console.error("ChannelIO script included twice.");}var ch=function(){ch.c(arguments);};ch.q=[];ch.c=function(args){ch.q.push(args);};w.ChannelIO=ch;function l(){if(w.ChannelIOInitialized){return;}w.ChannelIOInitialized=true;var s=document.createElement("script");s.type="text/javascript";s.async=true;s.src="https://cdn.channel.io/plugin/ch-plugin-web.js";var x=document.getElementsByTagName("script")[0];if(x.parentNode){x.parentNode.insertBefore(s,x);}}if(document.readyState==="complete"){l();}else{w.addEventListener("DOMContentLoaded",l);w.addEventListener("load",l);}})();
+            ChannelIO('boot', {
+              "pluginKey": "e2e09203-17ec-4a8c-98a3-d1eb81a456af"
+            });
+          `,
+                    }}
+                />
             </head>
             <body className="">{children}</body>
         </html>
