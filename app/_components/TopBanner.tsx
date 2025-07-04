987428c11fa6edf9d1fe6ace62535592bff4900e
ms-bang <ms.bang@teamsparta.co>
2025-07-04 21:27:15 +0900

    fix: change event name

--- app/_components/TopBanner.tsx
+++ app/_components/TopBanner.tsx
@@ -8,7 +8,7 @@ const APP_STORE_URL = 'https://apps.apple.com/kr/app/coffimer/id6747644924';
 
 export default function TopBanner() {
     const handleDownload = () => {
-        posthog.capture('my event', { property: 'value' });
+        posthog.capture('download_app_store');
         window.open(APP_STORE_URL, '_blank', 'noopener,noreferrer');
     };
 
